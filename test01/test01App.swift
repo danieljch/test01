a5a26a97e572c15ad08475b31e85940e7d6f23e7
Daniel Callisaya <daniel_jch@hotmail.com>
2024-01-12 12:38:12 -0400

    Initial Commit

---
+++ test01/test01App.swift
@@ -0,0 +1,17 @@
+//
+//  test01App.swift
+//  test01
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
